2e411de11aa09f701a041cddaba7f696f96ac7bd
nschaffner <schaffnn@oregonstate.edu>
2020-12-29 17:17:19 -0800

    First commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCQrSzzaAQSNi734ijtOcxsYUculR7XJac",
+    authDomain: "glacier-da797.firebaseapp.com",
+    projectId: "glacier-da797",
+    storageBucket: "glacier-da797.appspot.com",
+    messagingSenderId: "197504501345",
+    appId: "1:197504501345:web:06dca55aecc572ef2bcc20",
+    measurementId: "G-JLJCEX8JC6"
+  };
